255beda72f96472d1048963d7b544cb26ca2a97a
Gary Hughes <garyedwardhughes@gmail.com>
2019-04-27 12:06:55 +1000

    Unit 1

---
+++ MyFirstSharpApp/MyFirstSharpApp/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let items = argv.Length
+    printfn "Passed in %d items: %A" items argv
+    0 // return an integer exit code
---
+++ MyFirstSharpApp/MyFirstSharpApp/scratchpad.fsx
@@ -0,0 +1,17 @@
+﻿
+let text = "Hello, World"
+text.Length
+
+
+let greetPerson name age = sprintf "Hello, %s. You are %d years old" name age
+let greeting = greetPerson "Gary" 44
+
+let countWords (text:string) = text.Split([|' '|], System.StringSplitOptions.RemoveEmptyEntries).Length;;
+let input = "Hello There World";;
+let count = countWords input;;
+let file = new System.IO.StreamWriter("c:\\temp\\output.txt");;
+file.WriteLine(input);;
+file.WriteLine(count);;
+file.Close();;
+
+
