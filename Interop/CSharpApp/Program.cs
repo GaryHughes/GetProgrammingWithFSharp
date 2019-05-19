50e79ca7a7f5faa6d88800398ef15dc76ba6bee7
Gary Hughes <garyedwardhughes@gmail.com>
2019-05-19 10:56:08 +1000

    Interop.

---
+++ Interop/CSharpApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Model.Car(4, "Supacars", Tuple.Create(2.0, 4.0));
+
+            var bike = Model.Vehicle.NewMotorbike("CR", 250);
+
+            if (bike.IsMotorbike)
+            {
+                var motorBike = (Model.Vehicle.Motorbike)bike;
+                var engineSize = motorBike.EngineSize;
+            }
+
+            var subaru = Model.Functions.CreateCar(4, "Subaru", 2, 4);
+
+            var tesla = Model.Functions.CreateFourWheeledCar.Invoke("Tesla").Invoke(2.0).Invoke(4.0);
+
+
+
+        }
+    }
+}
---
+++ Interop/FSharpCode/Library.fs
@@ -0,0 +1,18 @@
+﻿namespace Model
+
+type Car = 
+    {   Wheels : int
+        Brand : string
+        Dimensions : float * float  }
+
+type Vehicle = 
+    | MotorCar of Car
+    | Motorbike of Name:string * EngineSize:float 
+
+module Functions =
+    
+    let CreateCar wheels brand x y =
+        {   Wheels = wheels; Brand = brand; Dimensions = x,y }
+
+    let CreateFourWheeledCar = CreateCar 4
+
